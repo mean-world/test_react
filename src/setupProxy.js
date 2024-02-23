72edbda17e07304ffe12034693bf1e08e97098ac
mean-world <g1m2a3i4l58832@gmail.com>
2024-02-23 10:08:08 +0800

    2/23

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,17 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/api', 
-    createProxyMiddleware({
-      target: 'http://backend-svc.website-system:5000', //注意沒有api在這裡
-      changeOrigin: true,
-    })
-  );
+//   app.use(
+//     '/api', 
+//     createProxyMiddleware({
+//       target: 'http://backend-svc.website-system:5000', //注意沒有api在這裡
+//       changeOrigin: true,
+//     })
+//   );
+
+app.use((req, res) => {
+    // 允許所有的來源
+    res.append('Access-Control-Allow-Origin', '*')
+    res.json({ status: 'success', message: 'ok' })
+  })
 };
